856da5d157be29f6f57e3eaa32af9963238e4f69
mikoto2000 <mikoto2000@gmail.com>
2013-10-14 01:14:58 +0900

    Added 10020_CountingCharacters.

---
+++ 100/10020_CountingCharacters/CountingCharacters.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <ctype.h>
+#include <memory.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    // initialize count of char.
+    int count[25];
+    for (int i = 0; i < 26; i++) {
+        count[i] = 0;
+    }
+
+    // count chars.
+    char c;
+    while (cin >> c) {
+
+        // modify caractor to lower case.
+        c = tolower(c);
+
+        // check char and inclement counter.
+        int index = c - 'a';
+        if (index >= 0
+                && index <= 26) {
+            count[index]++;
+        }
+    }
+
+    // print result.
+    for (int i = 0; i < 26; i++) {
+        char c = i + 'a';
+        cout << c << " : " << count[i] << endl;
+    }
+
+    return 0;
+}
+
