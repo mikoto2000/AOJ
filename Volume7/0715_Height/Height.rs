4da9e59265f72eed0d2571bb17bd430718864b67
mikoto2000 <mikoto2000@gmail.com>
2024-08-05 00:06:59 +0900

    Added Height.

---
+++ Volume7/0715_Height/Height.rs
@@ -0,0 +1,19 @@
+use std::str::FromStr;
+
+fn main() {
+    let mut line1 = String::new();
+    std::io::stdin().read_line(&mut line1).ok();
+    line1 = line1.trim().parse::<String>().ok().unwrap();
+    let line1: Vec<&str> = line1.split_whitespace().collect();
+    let a: u32 = FromStr::from_str(&line1[0]).unwrap();
+
+    let mut line2 = String::new();
+    std::io::stdin().read_line(&mut line2).ok();
+    line2 = line2.trim().parse::<String>().ok().unwrap();
+    let line2: Vec<&str> = line2.split_whitespace().collect();
+    let b: u32 = FromStr::from_str(&line2[0]).unwrap();
+
+    let diff = b - a;
+
+    println!("{}", diff);
+}
