afbd98b9400c380a04ddd22b8948f2fca76d2424
mikoto2000 <mikoto2000@gmail.com>
2024-10-12 06:09:44 +0000

    1_4_A A/B Problem.

---
+++ rust/ITPL/1_4_A_A_B_Problem/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    use std::str::FromStr;
+
+    let mut line1 = String::new();
+    std::io::stdin().read_line(&mut line1).ok();
+    line1 = line1.trim().parse::<String>().ok().unwrap();
+    let line1: Vec<&str> = line1.split_whitespace().collect();
+    let a: u32 = FromStr::from_str(&line1[0]).unwrap();
+    let b: u32 = FromStr::from_str(&line1[1]).unwrap();
+
+    println!("{} {} {}", a / b, a % b, a as f64 / b as f64);
+}
