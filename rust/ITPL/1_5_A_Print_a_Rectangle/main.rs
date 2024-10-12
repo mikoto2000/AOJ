423f841f56e972f09b81051d0b823039b68c5925
mikoto2000 <mikoto2000@gmail.com>
2024-10-12 06:38:32 +0000

    1_5_A_Print_a_Rectangle.

---
+++ rust/ITPL/1_5_A_Print_a_Rectangle/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    use std::str::FromStr;
+
+    loop {
+
+        let mut line1 = String::new();
+        std::io::stdin().read_line(&mut line1).ok();
+        line1 = line1.trim().parse::<String>().ok().unwrap();
+        let line1: Vec<&str> = line1.split_whitespace().collect();
+        let h: u32 = FromStr::from_str(&line1[0]).unwrap();
+        let w: u32 = FromStr::from_str(&line1[1]).unwrap();
+
+        if h == 0 && w == 0 {
+            break;
+        }
+
+        for _ in 0..h {
+            for _ in 0..w {
+                print!("#");
+            }
+            println!();
+        }
+
+        println!();
+    }
+}
